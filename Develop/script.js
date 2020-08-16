efc4b4856af54706fa83954d9cd23cccc18c5660
Jake Fullmer <w.jake.fullmer@gmail.com>
2020-08-16 16:56:33 -0600

    Created functions to check if boxes are checked or not

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,20 @@
 // Assignment code here
-function generatePassword() {
 
+ /* function generatePassword(lower, upper, number, symbol, length) {
+  //1. initialize password variable
+  //2. Filter out unchecked types
+  //3. Loop over length, call generator function for each type
+  //4. add final password to the password variable and return. 
+
+  let generatePassword = '';
+
+  const typesCount = lower + upper + number + symbol;
+
+  console.log('types count: ', typesCount);
+
+  const typesArr = [lower, upper, number, symbol];
+
+  console.log('typesArr: ', typesArr);
 }
 
 
@@ -28,8 +42,60 @@ generateBtn.addEventListener("click", writePassword);
 // 5. When I answer prompt, input should be validated and at least 1 character type selected.
 // 6. When all prompts answered, paddword is generated that matches criteria
 // 7. Final password is displayed in an alert or written to the page. 
+ 
+*/
+
+// DOM elements
+const resultEL = document.getElementById('result');
+const lengthEL = document.getElementById('length');
+const uppercaseEL = document.getElementById('uppercase');
+const lowercaseEL = document.getElementById('lowercase');
+const numbersEL = document.getElementById('numbers');
+const symbolsEL = document.getElementById('symbols');
+const generateEL = document.getElementById('generate');
+const clipboardEL = document.getElementById('clipboard');
 
 
+// turns the generator functions into objects
+const randomFunc = {
+  // keys
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+};
+
+// Generate event listen
+generateEL.addEventListener('click', () => {
+  //  length to a number instead of string
+  const length = +lengthEL.value;
+  // checks to see if boxes are checked
+  const hasLower = lowercaseEL.checked;
+  const hasUpper = uppercaseEL.checked;
+  const hasNumber = numbersEL.checked;
+  const hasSymbol = symbolsEL.checked;
+  resultEL.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
+
+// Generate password function
+function generatePassword(lower, upper, number, symbol, length) {
+  //1. initialize password variable
+  //2. Filter out unchecked types
+  //3. Loop over length, call generator function for each type
+  //4. add final password to the password variable and return. 
+
+  let generatePassword = '';
+
+  const typesCount = lower + upper + number + symbol;
+
+  console.log('types count: ', typesCount);
+
+  // creates an array of objects, using the keys from above
+  const typesArr = [{lower}, {upper}, {number}, {symbol}];
+
+  console.log('typesArr: ', typesArr);
+
+}
 
 
 //Generator functions
